07d63d61b89d118bac543d21eeec704addf95b65
Derp <Derp>
2021-07-17 22:16:50 -0700

    Speed change

--- src/IRouter.js
+++ src/IRouter.js
@@ -448,7 +448,7 @@
                     truckSpeed: 3600.0 / 50000.0 / .5, // 45 kmh (75% of 60kmh)
                     jeepSpeed: 3600.0 / 55000.0 / .5,
                     flatbedSpeed: 3600 / 25000.0 / .5,
-                    htdSpeed: 3600 / 14500 / .90 / .5,
+                    htdSpeed: 3600 / 14500 / .90 / .65,
 
                     pathFinder: new PathFinder(MainRoutes, {
                         compact: null,
